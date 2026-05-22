1afdfa8486f599a2c12b93b6cc9a76910564ddbe
nobumacsuzuki <nobumac.suzuki@gmail.com>
2026-05-22 00:35:59 -0700

    refactor task2

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,19 +1,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     char *name = NULL;
-    char message[100];
-    if (argc > 1)
+    char* message = NULL;
+    const char messagePrefix[] = "Hello World";
+    const char messageDelimiter[] = ", ";
+    const char messageSuffix[] = "!";
+    if (argc == 2)
     {
         name = argv[1];
-        sprintf(message, "Hello World, %s!", name);
+        size_t nameLength = strlen(name);
+        message = (char*)malloc(sizeof(char) * 
+            (sizeof(messagePrefix) 
+            + sizeof(messageDelimiter)
+            + strlen(name) 
+            + sizeof(messageSuffix)));
+        sprintf(message, "%s%s%s%s", messagePrefix, messageDelimiter, name, messageSuffix);
     }
     else
     {
-        sprintf(message, "Hello World!");
+        message = (char*)malloc(sizeof(char) * (sizeof(messagePrefix) + sizeof(messageSuffix)));
+        sprintf(message, "%s%s", messagePrefix, messageSuffix);
     }
     printf("%s\n", message);
     return EXIT_SUCCESS;    
